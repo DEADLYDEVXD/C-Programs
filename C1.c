ebb5b483579a152f15e0d721f79045a4a1756523
Dev <dev613006644@gmail.com>
2025-09-05 10:53:01 +0530

    Added 1 C program

---
+++ C1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    float num;
+    scanf("%f",&num);
+    int D1 = round(num);
+    printf("The converted integer value is %d\n.",D1);
+    return 0;
+}
