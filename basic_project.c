f4ebe36266a92a00f3185a6997f3f6eab0fb5a00
Dev <dev613006644@gmail.com>
2025-09-09 19:19:41 +0530

    Basic C Program
    done for collage

---
+++ basic_project.c
@@ -0,0 +1,49 @@
+// You are using GCC
+#include<stdio.h>
+
+int main()
+{
+    double inv_value;
+    double inv_value2;
+    int por_id;
+    
+    
+    scanf("%lf",&inv_value);
+    scanf("%lf",&inv_value2);
+    scanf("%d", &por_id);
+    
+    printf("The values in the reverse order: \n");
+    
+    
+    printf("%.2d\n",por_id);
+    printf("%.2lf\n",inv_value2);
+    printf("%.2lf\n",inv_value);
+//    printf("The values in the reverse order:")
+ /*   if (inv_value>inv_value2>por_id)
+    {
+        printf("The values in the reverse order:");
+    }
+    else if (inv_value>por_id>inv_value)
+    {
+        printf("The values in the reverse order:");
+    }
+    else if (inv_value2>inv_value>por_id)
+    {
+        printf("The values in the reverse order:");
+    }
+     else if (inv_value2>por_id>inv_value)
+    {
+        printf("The values in the reverse order:");
+    }
+    
+    else if (por_id>inv_value>inv_value2)
+    {
+        printf("The values in the reverse order:");
+    }
+    else
+    {
+         printf("The values in the reverse order:");
+    }
+*/
+
+}
